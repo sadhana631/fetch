3de5bab4d7809e584d37b4db870a8ae7aad0a73d
sadhana631 <indhusweety631@gmail.com>
2024-08-27 08:05:32 +0000

    fifth commit

--- src/components/BlogList/index.js
+++ src/components/BlogList/index.js
@@ -1,30 +1,44 @@
 import {Component} from 'react'
 import Loader from 'react-loader-spinner'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+
 import BlogItem from '../BlogItem'
+
+
 import './index.css'
+
 
 class BlogList extends Component {
   state = {isLoading: true, blogsData: []}
+  
+  
   componentDidMount() {
     this.getBlogsData()
   }
+  
+  
   getBlogsData = async () => {
     const response = await fetch('https://apis.ccbp.in/blogs')
     const data = await response.json()
-    console.log(data)
     const formattedData = data.map(eachItem => ({
       id: eachItem.id,
-      title: eachItem.image_url,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
       avatarUrl: eachItem.avatar_url,
       author: eachItem.author,
       topic: eachItem.topic,
     }))
+   
+   
     this.setState({blogsData: formattedData, isLoading: false})
   }
 
+
   render() {
     const {blogsData, isLoading} = this.state
+   
+   
     return (
       <div className="blogs-list-container">
         {isLoading ? (
